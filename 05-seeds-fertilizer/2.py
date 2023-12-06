e56e79f771b198e96b4868620de7b8f79b37014d
chadcat7 <namishpande2007@gmail.com>
2023-12-07 01:23:44 +0530

    0502

--- 05-seeds-fertilizer/2.py
+++ 05-seeds-fertilizer/2.py
@@ -1,24 +1,23 @@
 ## DISCLAIMER THIS CODE WILL CRASH YOUR SYSTEM. I DID THIS BY MANUALLY GETTING THE VALUES OF 2 RANGES AT A TIME AND THEN GETTING THEM MINIMUM AT THE END
-import numpy as np
-
 def seedy(seed, dick):
     n = seed
     v = ['seed-to-soil', 'soil-to-fertilizer', 'fertilizer-to-water', 'water-to-light', 'light-to-temperature', 'temperature-to-humidity', 'humidity-to-location']
     for d in v:
         value = 0
         for i in dick[d]:
-            if i[1] <= n <= i[1] + i[2]:
+            if n >= i[1] and n <= i[1] + i[2]:
                 value = n - i[1] + i[0]
                 n = value
                 break
     return n
 
+
 def calcSeeds(seeds):
-    return np.concatenate([np.arange(i[0], i[0] + i[1]) for i in zip(seeds[::2], seeds[1::2])])
+    return [num for start, length in zip(seeds[::2], seeds[1::2]) for num in range(start, start + length)]
 
 def calc(file):
     maps = [line.strip() for line in open(file) if line.strip()]
-    seeds = np.array([int(i) for i in maps[0].split(": ")[1].split(" ")])
+    seeds = [int(i) for i in maps[0].split(": ")[1].split(" ")]
     seeds = calcSeeds(seeds)
     thedata = maps[1:]
     dick = {}
@@ -28,10 +27,12 @@ def calc(file):
             curr = i.split(" ")[0]
             dick[curr] = []
         else:
-            dick[curr].append(np.array([int(x) for x in i.split()]))
-
-    b = np.array([seedy(seed, dick) for seed in seeds])
-    return np.min(b)
+            dick[curr].append([int(i) for i in i.split()])
+    b = []
+    for seed in seeds:
+        b.append(seedy(seed, dick))
+    b.sort()
+    return b[0]
 
 def main():
     ans = calc('inputs.txt')
