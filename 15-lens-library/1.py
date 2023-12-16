afdfcd9743305f0ad102cb16738998b76079bfc2
chadcat7 <namishpande2007@gmail.com>
2023-12-16 13:50:29 +0530

    1501

---
+++ 15-lens-library/1.py
@@ -0,0 +1,18 @@
+def calc(lenses):
+    lenses = [line.strip().split(",") for line in open(lenses)][0]
+    print(lenses)
+    values = []
+    for lens in lenses:
+        val = 0
+        for char in lens:
+            a = ord(char)
+            val = val+a
+            val = val * 17 
+            val = val % 256
+        values.append(val)
+    print(sum(values))
+
+def main():
+    calc("inputs.txt")
+
+main()
